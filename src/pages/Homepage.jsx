2e89a0881049e9e1bc6bb253b1ab3e16eb294810
farhan <mohdfarhanfahrezy@gmail.com>
2023-08-25 00:50:35 +0700

    feat : filtering event

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
 import imageHome from "../assets/imgHomepage.png";
-
 import EventList from "../Lists/EventList";
+import axios from "../api/axios";
 
 function Homepage() {
+  const [filter, setFilter] = useState({
+    namaEvent: "",
+    kategori: "",
+    tanggal: "",
+    lokasi: "",
+  });
+  const [listKategori, setlistKategori] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      axios
+        .get(`/categories/`)
+        .then((res) => {
+          setlistKategori([...res.data.data]);
+        })
+        .catch((err) => {
+          console.log(err);
+          setlistKategori([]);
+        });
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <section className="h-[80vh]">
       <img
@@ -24,6 +49,10 @@ function Homepage() {
                 type="text"
                 id="nameEvent"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 d"
+                onChange={(e) => {
+                  setFilter({ ...filter, namaEvent: e.target.value });
+                }}
+                value={filter.namaEvent}
               />
             </div>
             <div className="flex-grow">
@@ -34,12 +63,20 @@ function Homepage() {
                 Kategori
               </label>
               <select
-                defaultValue={null}
-                id="category"
+                defaultValue={""}
+                value={filter.kategori}
+                id="location"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                onChange={(e) => {
+                  setFilter({ ...filter, kategori: e.target.value });
+                }}
               >
-                <option>pilih kategori</option>
-                <option value="US">United States</option>
+                <option value={""}>Pilih kategori</option>
+                {listKategori.map((kategori, index) => (
+                  <option key={index} value={kategori.name}>
+                    {kategori.name}
+                  </option>
+                ))}
               </select>
             </div>
             <div className="flex-grow">
@@ -53,29 +90,34 @@ function Homepage() {
                 type="date"
                 id="date"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 d"
+                onChange={(e) => {
+                  setFilter({ ...filter, tanggal: e.target.value });
+                }}
+                value={filter.tanggal}
               />
             </div>
             <div className="flex-grow">
               <label
                 htmlFor="location"
-                className="block mb-2 text-sm font-medium text-gray-900 "
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
-                Kategori
+                Lokasi
               </label>
-              <select
-                defaultValue={null}
+              <input
+                type="text"
                 id="location"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-              >
-                <option>pilih kategori</option>
-                <option value="US">United States</option>
-              </select>
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 d"
+                onChange={(e) => {
+                  setFilter({ ...filter, lokasi: e.target.value });
+                }}
+                value={filter.lokasi}
+              />
             </div>
           </form>
         </div>
       </div>
       <div className="md:px-16 py-3 md:py-20 bg-gray-200">
-        <EventList />
+        <EventList filter={filter} />
       </div>
     </section>
   );
