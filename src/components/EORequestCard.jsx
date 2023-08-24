f745f94fade8247b03d2e47d85e81914f9262d83
farhan <mohdfarhanfahrezy@gmail.com>
2023-08-24 12:51:54 +0700

    feat: eos approve

--- src/components/EORequestCard.jsx
+++ src/components/EORequestCard.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-hot-toast";
+// import { useNavigate } from "react-router-dom";
+import axios from "../api/axios";
 
-const EORequestCard = ({ name, email, dateOfRequest, status }) => {
+const EORequestCard = ({ name, email, dateOfRequest, user_id }) => {
+  const [loadingApprove, setloadingApprove] = useState(false);
+  const [statusUpdate, setstatusUpdate] = useState(null);
+
   const handleApproveButton = () => {
     // do something here
-    toast.success("Approved!");
+    setloadingApprove(true);
+    const data = {
+      user_id,
+      status: "ACTIVE",
+    };
+    axios
+      .post("/auth/verify", data)
+      .then(() => {
+        toast.success("Berhasil menyetujui");
+        setstatusUpdate(true);
+      })
+      .catch((err) => {
+        toast.error("Proses gagal");
+      })
+      .finally(setloadingApprove(false));
   };
 
   const handleRejectButton = () => {
     // do something here
-    toast.success("Rejected!");
+    setloadingApprove(true);
+    const data = {
+      user_id,
+      status: "REJECTED",
+    };
+    axios
+      .post("/auth/verify", data)
+      .then(() => {
+        toast.success("Berhasil menolak");
+        setstatusUpdate(false);
+      })
+      .catch((err) => {
+        toast.error("Proses gagal");
+      })
+      .finally(setloadingApprove(false));
   };
   return (
     <div className="flex flex-row justify-between items-center bg-white w-full rounded-md py-2 px-4 shadow-md">
@@ -25,30 +58,36 @@ const EORequestCard = ({ name, email, dateOfRequest, status }) => {
           <p>Tanggal Pengajuan : {dateOfRequest}</p>
         </div>
       </div>
-      {status === "INACTIVE" ? (
+      {statusUpdate === null ? (
         <div className="flex flex-row justify-center items-center gap-2">
-          <button
-            className="hover:scale-105 active:scale-95 transition-all"
-            onClick={handleApproveButton}
-          >
-            <img
-              src="/icons/approve-icon.png"
-              alt="Approve"
-              className="w-[30px]"
-            />
-          </button>
-          <button
-            className="hover:scale-105 active:scale-95 transition-all"
-            onClick={handleRejectButton}
-          >
-            <img
-              src="/icons/reject-icon.png"
-              alt="Approve"
-              className="w-[30px]"
-            />
-          </button>
+          {loadingApprove ? (
+            <div>Loading</div>
+          ) : (
+            <>
+              <button
+                className="hover:scale-105 active:scale-95 transition-all"
+                onClick={handleApproveButton}
+              >
+                <img
+                  src="/icons/approve-icon.png"
+                  alt="Approve"
+                  className="w-[30px]"
+                />
+              </button>
+              <button
+                className="hover:scale-105 active:scale-95 transition-all"
+                onClick={handleRejectButton}
+              >
+                <img
+                  src="/icons/reject-icon.png"
+                  alt="Approve"
+                  className="w-[30px]"
+                />
+              </button>
+            </>
+          )}
         </div>
-      ) : status === "ACTIVE" ? (
+      ) : statusUpdate ? (
         <p className="text-green-400 text-[14px] font-bold">Disetujui</p>
       ) : (
         <p className="text-red-400 text-[14px] font-bold">Ditolak</p>
