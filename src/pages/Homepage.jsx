54400ea7b6a78578e55ccbfb458ee910d6dc1e20
farhan <mohdfarhanfahrezy@gmail.com>
2023-08-25 17:12:10 +0700

    fix : category select on homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -63,7 +63,6 @@ function Homepage() {
                 Kategori
               </label>
               <select
-                defaultValue={""}
                 value={filter.kategori}
                 id="location"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
