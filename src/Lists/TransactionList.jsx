7bb232247e3cf319d896afe0e9449411cbcaf3e6
farhan <mohdfarhanfahrezy@gmail.com>
2023-08-26 19:09:41 +0700

    fix : add date order in riwayat transaksi

--- src/Lists/TransactionList.jsx
+++ src/Lists/TransactionList.jsx
@@ -2,24 +2,50 @@ import React, { useEffect, useState } from "react";
 import TransactionCard from "../components/TransactionCard";
 import axios from "../api/axios";
 
-function TransactionList() {
+function TransactionList({ dateOrder }) {
   const [data, setData] = useState([]);
+
   useEffect(() => {
     axios
       .get("/transactions/my")
       .then((res) => {
-        setData(res.data.data);
+        // const newData = [...res.data.data]
+
+        setData(changeOrder([...res.data.data], dateOrder));
       })
       .catch((err) => {
         console.log(err.response);
       });
-  }, []);
-  // console.log(data)
+  }, [dateOrder]);
+
+  const changeOrder = (data, str) => {
+    if (str === "Terlama") {
+      let newTransaction = [...data];
+      newTransaction.sort((a, b) => {
+        const dateA = new Date(a.created_at);
+        const dateB = new Date(b.created_at);
+        return dateA - dateB;
+      });
+      return newTransaction;
+    } else {
+      let newTransaction = [...data];
+      newTransaction.sort((a, b) => {
+        const dateA = new Date(a.created_at);
+        const dateB = new Date(b.created_at);
+        return dateB - dateA;
+      });
+      return newTransaction;
+    }
+  };
   return (
     <div className="flex flex-col justify-center items-center w-full gap-3">
-      {data.map((item) => {
-        return <TransactionCard {...item} key={item.transaction_id} />;
-      })}
+      {data.length !== 0 ? (
+        data.map((item) => {
+          return <TransactionCard {...item} key={item.transaction_id} />;
+        })
+      ) : (
+        <p className="text-center">Kamu tidak memiliki riwayat transaksi</p>
+      )}
     </div>
   );
 }
--- src/pages/TiketPage.jsx
+++ src/pages/TiketPage.jsx
@@ -1,51 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import Tiket from '../components/Tiket'
-import axios from '../api/axios'
-import ModalsTicketInfo from '../components/ModalsTicketInfo'
+import React, { useEffect, useState } from "react";
+import Tiket from "../components/Tiket";
+import axios from "../api/axios";
+import ModalsTicketInfo from "../components/ModalsTicketInfo";
 
 function TiketPage() {
-  const [show,setShow] = useState(false)
-  const [selected, setSelected] = useState()
-  const [ data , setData ] = useState([])
-  
-  useEffect(()=>{
-    axios
-    .get('/tickets/my')
-    .then((res)=>{
-      setData(res.data.data)
-    })
-    .catch((err)=>{
-      console.log(err.response)
-    })
-  },[])
+  const [show, setShow] = useState(false);
+  const [selected, setSelected] = useState();
+  const [data, setData] = useState([]);
 
-  function ticketDetail(data){
-    setSelected(data)
-    setShow(true)
+  useEffect(() => {
+    axios
+      .get("/tickets/my")
+      .then((res) => {
+        setData(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, []);
+
+  function ticketDetail(data) {
+    setSelected(data);
+    setShow(true);
   }
 
   return (
-    <section className='py-3 px-5'>
-      <h5 className='text-xl border-b-2 border-gray-300'>Upcoming Event</h5>
-      <div className='lg:mx-48 my-10 relative'>
-        {data.map((item)=>{
-          return(
-            <div className='hover:shadow-md cursor-pointer' key={item.ticket_id} onClick={()=>ticketDetail(item)}>
-              <Tiket {...item}/>
-            </div>
-          )
-        })}
-
-        {show && (
-          <ModalsTicketInfo
-          data={selected}
-          setShow={setShow}
-          />
+    <section className="py-3 px-5">
+      <h5 className="text-xl border-b-2 border-gray-300">Upcoming Event</h5>
+      <div className="lg:mx-48 my-10 relative">
+        {data.length !== 0 ? (
+          data.map((item) => {
+            return (
+              <div
+                className="hover:shadow-md cursor-pointer"
+                key={item.ticket_id}
+                onClick={() => ticketDetail(item)}
+              >
+                <Tiket {...item} />
+              </div>
+            );
+          })
+        ) : (
+          <p className="text-center">Kamu tidak memiliki tiket</p>
         )}
 
+        {show && <ModalsTicketInfo data={selected} setShow={setShow} />}
       </div>
     </section>
-  )
+  );
 }
 
-export default TiketPage
+export default TiketPage;
