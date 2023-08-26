249e94b907c8baf748103df85f89efb3409e6838
Apriana <aprianacahya04@gmail.com>
2023-08-26 22:41:39 +0700

    merge

--- src/Lists/TransactionList.jsx
+++ src/Lists/TransactionList.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 import TransactionCard from "../components/TransactionCard";
 
-function TransactionList() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axios
-      .get("/transactions/my")
-      .then((res) => {
-        setData(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err.response);
-      });
-  }, []);
-const TransactionList = ({ transactionHistory }) => {
+function TransactionList({ transactionHistory }) {
   return (
     <>
       {transactionHistory.map((history) => (
